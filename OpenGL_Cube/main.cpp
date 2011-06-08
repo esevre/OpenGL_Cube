cadba1df2aa7cf6c5ec485e8b0e9b330188bcebb
Erik Sevre <esevre@gmail.com>
2011-06-08 18:59:45 -0500

    Initial Commit

---
+++ OpenGL_Cube/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Cube
+//
+//  Created by Erik Sevre on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
